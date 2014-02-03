768b206c8afd2687ffc6c1d10b31471830088dad
drtyhbo <abinnewies@gmail.com>
2014-02-03 12:56:38 -0800

    + Add database information to the settings.py file.

--- demo-webapps/webapps/settings.py
+++ demo-webapps/webapps/settings.py
@@ -11,13 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'live_mockup',
+        'USER': 'django',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
