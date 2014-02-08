eacbfd2951ed5a77ce63cb1f64acaed61845d7f9
Andreas Binnewies <andreasbinnewies@andreas-binnewiess-imac.local>
2014-02-08 09:03:34 -0800

    + Update Access-Control-Allow-Origin header to pass * rather than null.

--- demos/prototype/middleware.py
+++ demos/prototype/middleware.py
@@ -1,4 +1,4 @@
 class HeadersMiddleware(object):
   def process_response(self, request, response):
-    response['Access-Control-Allow-Origin'] = 'null'
+    response['Access-Control-Allow-Origin'] = '*'
     return response
